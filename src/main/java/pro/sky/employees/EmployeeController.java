41a318090f495fcae263eb44ac95849602f8f0e4
Katrin_skt <katrinpivzavod@mail.ru>
2024-04-26 21:39:39 +0300

    Done

--- src/main/java/pro/sky/employees/EmployeeController.java
+++ src/main/java/pro/sky/employees/EmployeeController.java
@@ -6,37 +6,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
 import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
-    private final EmployeeService employeeService;
+    private final EmployeeServiceImpl employeeService;
 
-    public EmployeeController(EmployeeService employeeService) {
+    public EmployeeController(EmployeeServiceImpl employeeService) {
         this.employeeService = employeeService;
     }
 
     @GetMapping("/add")
-    public Employee addNewEmployee(@RequestParam(required = false, name = "firstName") String firstName,
-                                   @RequestParam(required = false, name = "lastName") String lastName) throws EmployeeAlreadyAddedException, EmployeeStorageIsFullException {
-        return employeeService.addNewEmployee(firstName, lastName);
+    public Employee addNewEmployee(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.add(firstName, lastName);
     }
 
     @GetMapping("/remove")
-    public Employee removeEmployee(@RequestParam(required = false, name = "firstName") String firstName,
-                                   @RequestParam(required = false, name = "lastName") String lastName) throws EmployeeNotFoundException {
-        return employeeService.removeEmployee(firstName, lastName);
+    public Employee removeEmployee(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.remove(firstName, lastName);
     }
 
     @GetMapping("/find")
-    public Employee findEmployee (@RequestParam(required = false, name = "firstName") String firstName,
-                                  @RequestParam(required = false, name = "lastName") String lastName) throws EmployeeNotFoundException {
-        return employeeService.findEmployee(firstName, lastName);
+    public Employee findEmployee (@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.find(firstName, lastName);
     }
-    @GetMapping("/employees")
-    public List<Employee> getEmployees (){
-        return this.employeeService.getEmployees();
+    @GetMapping
+    public Collection<Employee> findAll (){
+        return employeeService.findAll();
     }
 
 }
